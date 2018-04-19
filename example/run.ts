3440f8f5c80a51c7b2d680a6dcd33053c353d0d1
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-04-19 11:43:32 +0100

    Refactor cli to an example project

---
+++ example/run.ts
@@ -0,0 +1,5 @@
+import { init } from '../dist';
+
+const DATA_PATH: string = require('./data/path.js');
+
+init(1337, DATA_PATH);
--- src/cli.ts
+++
@@ -1,5 +0,0 @@
-import { init } from './init';
-
-const DATA_PATH: string = require('../data/path.js');
-
-init(1337, DATA_PATH);
