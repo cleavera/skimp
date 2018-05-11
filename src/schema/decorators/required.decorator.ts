f7c5f86675d457b9a6e7350a11cbe299f2ea9a90
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-05-11 11:24:07 +0100

    treat empty strings as value

--- src/schema/decorators/required.decorator.ts
+++ src/schema/decorators/required.decorator.ts
@@ -3,7 +3,7 @@ import { ValidationFieldRequiredException } from '../exceptions/validation-field
 
 export function Required(target: any, propertyKey: string): void {
     SCHEMA_REGISTER.addValidation(target.constructor, (model: any) => {
-        if ((!(propertyKey in model)) || model[propertyKey] === void 0 || model[propertyKey] === null || model[propertyKey] === '') {
+        if ((!(propertyKey in model)) || model[propertyKey] === void 0 || model[propertyKey] === null) {
             throw new ValidationFieldRequiredException(propertyKey, model);
         }
     });
