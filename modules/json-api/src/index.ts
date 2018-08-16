2b09619738ea9fb1a3cbaeb0b096e549631a808c
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-08-16 10:55:14 +0100

    Export json schema interfaces from json-api module

--- modules/json-api/src/index.ts
+++ modules/json-api/src/index.ts
@@ -1,3 +1,10 @@
+export { IAttributes } from './interfaces/attributes.interface';
+export { ILinks } from './interfaces/links.interface';
+export { IRelationship } from './interfaces/relationship.interface';
+export { IJsonData } from './interfaces/json-data.interface';
+export { IJsonError } from './interfaces/json-error.interface';
+export { IJsonErrors } from './interfaces/json-errors.interface';
+export { Serialiser } from './classes/serialiser';
 export { ISchemaObject } from './interfaces/schema-object.interface';
 export { ISchemaRoot } from './interfaces/schema-root.interface';
 export { IJsonApi } from './interfaces/json-api.interface';
