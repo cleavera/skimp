48771f8b8f491e8b8863e1762cfb2cdff7f918a2
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2019-07-15 11:16:52 +0100

    refactor(http): remove stream from response interface

--- modules/http/src/interfaces/response.interface.ts
+++ modules/http/src/interfaces/response.interface.ts
@@ -1,4 +1,3 @@
-import { Writable } from 'stream';
 import { ResponseCode } from '../constants/response-code.constant';
 import { ResponseType } from '../constants/response-types.constant';
 
@@ -6,7 +5,6 @@ export interface IResponse {
     statusCode: number;
     corsHeader: string | Array<string>;
     location: string;
-    stream: Writable;
 
     setAllow(post: boolean, put: boolean, remove: boolean): void;
 
