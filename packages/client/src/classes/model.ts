a652a4250b864c6f3004679e92e6a5232aeab38b
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2022-01-19 15:35:25 +0000

    feat(client): add ability to clone models

--- packages/client/src/classes/model.ts
+++ packages/client/src/classes/model.ts
@@ -1,9 +1,22 @@
 import { isNull } from '@cleavera/utils';
 import { MODEL_REGISTER, ResourceLocation } from '@skimp/core';
+import { Serialiser } from '@skimp/json-api';
 import { NoLocationRegisteredException } from '@skimp/router';
 import { ISchema, SCHEMA_REGISTER, SchemaNotRegisteredException } from '@skimp/schema';
 
 export class Model {
+    public static clone<T extends object>(model: T): T {
+        const serialiser: Serialiser = new Serialiser();
+        const location: ResourceLocation | null = MODEL_REGISTER.getLocation(model);
+        const clonedModel: T = serialiser.deserialise(JSON.parse(serialiser.serialiseModel(model))) as T;
+
+        if (location !== null) {
+            MODEL_REGISTER.setLocation(clonedModel, location);
+        }
+
+        return clonedModel;
+    }
+
     public static getLocation(model: object): ResourceLocation | null {
         return MODEL_REGISTER.getLocation(model);
     }
